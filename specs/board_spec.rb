30441d92ff610f2b6d64dd5bd2bae0215bb821ff
Charlie Wood <charliemowood@gmail.com>
2018-03-22 02:35:23 +0100

    Add test verifying square and piece functionality

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -6,7 +6,7 @@ class TestBoard < Minitest::Test
   def setup
     @board = Board.new()
     @board_setup = [{square: "a1", piece_status: "white_castle_one"}, {square: "a2", piece_status:
-      "white_knight_one"}, {square: "a3", piece_status: "white_bishop_one"}]
+      "white_knight_one"}, {square: "a3", piece_status: "white_bishop_one"}, square: "a4", piece_status: "white_king"]
   end
 
   def test_board_exists
@@ -23,6 +23,10 @@ class TestBoard < Minitest::Test
 
   def test_board_has_bishop_at_at_a3
     assert_equal(@board.position(@board_setup, "a3"), "white_bishop_one")
+  end
+
+  def test_board_has_bishop_at_at_a4
+    assert_equal(@board.position(@board_setup, "a4"), "white_king")
   end
 
 end
